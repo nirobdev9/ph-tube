8de32d3004c8f4f0017aaca167f41e14c71e837a
nirobdev9 <smnnirob839@gmail.com>
2025-03-15 13:10:21 +0600

    added loadVideoByCategories

--- js/videoapi.js
+++ js/videoapi.js
@@ -9,51 +9,32 @@ const loadVideo = () => {
     .then((res) => res.json())
     .then((data) => displayVideo(data.videos));
 };
-// category
-// :
-// "Music"
-// category_id
-// :
-// "1001"
+const loadVideoByCategories = (id) =>{
+    
+    const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+    console.log(url)
+    fetch(url).then((res) => res.json()).then((data)=> displayVideo(data.category))
+}
+ 
 function displayCategories(categories) {
   // get btn container
   const buttonContainer = document.getElementById("btn-container");
   // loop operation on array
   for (cat of categories) {
+    console.log(cat)
     // create new element
     const btnContainerDiv = document.createElement("div");
     btnContainerDiv.innerHTML = `
-    <button class="btn hover:bg-[#FF1F3D] hover:text-white"> ${cat.category}</button>
+    <button onclick="loadVideoByCategories(${cat.category_id})" class="btn hover:bg-[#FF1F3D] hover:text-white"> ${cat.category}</button>
     `;
     // append
     buttonContainer.append(btnContainerDiv);
   }
 }
-// {category_id: '1001', video_id: 'aaag', thumbnail: 'https://i.ibb.co/DRxB1Wm/sunris.jpg', title: 'Sunrise Reverie', authors: Array(1), …}
-// authors
-// : 
-// [{…}]
-// category_id
-// : 
-// "1001"
-// description
-// : 
-// "'Sunrise Reverie' by Ava Johnson takes listeners on a serene journey through tranquil melodies and soft harmonies. With 1.1K views, this track is perfect for morning relaxation or an evening wind-down. Ava's heartfelt lyrics and soothing voice create a sense of peace, making it a go-to for fans seeking calm and inspiration in their musical choices."
-// others
-// : 
-// {views: '1.1K', posted_date: '16950'}
-// thumbnail
-// : 
-// "https://i.ibb.co/DRxB1Wm/sunris.jpg"
-// title
-// : 
-// "Sunrise Reverie"
-// video_id
-// : 
-// "aaag"
 
 const displayVideo = (videos) => {
   const videoContainer = document.getElementById("videos-container");
+  videoContainer.innerHTML = "";
   videos.forEach((video) => {
     console.log(video);
     const videoDiv = document.createElement("div");
@@ -74,7 +55,7 @@ const displayVideo = (videos) => {
 
                 </div>
                 <div>
-                    <h2 class="card-title font-bold">wordpress tutorial</h2>
+                    <h2 class="card-title font-bold">${video.title}</h2>
                     <div class="flex gap-3"> <p class="text-sm text-gray-600">${video.authors[0].profile_name}</p>
                         <img class="w-5" src="./assets/icons8-verified-badge-32.png" alt="">
                     </div>
@@ -95,4 +76,4 @@ const displayVideo = (videos) => {
 };
 
 loadCategories();
-loadVideo();
+ 
