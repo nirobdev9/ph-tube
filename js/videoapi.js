c2b3825683c984d72ee4a4924e615b1cebf6aa7d
nirobdev9 <smnnirob839@gmail.com>
2025-03-16 14:36:20 +0600

    completed ph tube

--- js/videoapi.js
+++ js/videoapi.js
@@ -1,33 +1,80 @@
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("videos-container").classList.add("hidden");
+
+}
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("videos-container").classList.remove("hidden");
+
+}
+
+function removeActive (){
+  const activeBtn = document.getElementsByClassName("active");
+  for (let btn of activeBtn){
+    btn.classList.remove("active")
+  }
+}
+
 function loadCategories() {
   // fetch the data
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories));
 }
-const loadVideo = () => {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+const loadVideo = (searchText = "") => {
+  showLoader()
+  fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res) => res.json())
-    .then((data) => displayVideo(data.videos));
+    .then((data) => {
+      removeActive();
+      document.getElementById("btn-all").classList.add("active");
+      displayVideo(data.videos)
+    });
 };
 const loadVideoByCategories = (id) =>{
     
     const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
-    console.log(url)
     fetch(url).then((res) => res.json()).then((data)=> {
       const buttonClicked = document.getElementById(`btn-${id}`)
+      removeActive ()
       buttonClicked.classList.add("active")
-      console.log(buttonClicked)
       displayVideo(data.category)
 
     })
 }
+const loadVideoByDetail = (id) =>{
+  const url = "https://openapi.programming-hero.com/api/phero-tube/video/aaac";
+  fetch(url).then((res) => res.json()).then((data)=>displayVideoDetail(data.video))
+} 
+const displayVideoDetail = (video) => {
+   document.getElementById("video_details").showModal()
+   const videoDetailContainer = document.getElementById("details-container");
+   videoDetailContainer.innerHTML = `
+   <div class="card bg-base-100 image-full w-96 shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+   
+   `
+
+}
+ 
  
 function displayCategories(categories) {
   // get btn container
   const buttonContainer = document.getElementById("btn-container");
   // loop operation on array
   for (cat of categories) {
-    console.log(cat)
     // create new element
     const btnContainerDiv = document.createElement("div");
     btnContainerDiv.innerHTML = `
@@ -52,7 +99,8 @@ const displayVideo = (videos) => {
     return;
   }
   videos.forEach((video) => {
-    console.log(video);
+     
+    
     const videoDiv = document.createElement("div");
     videoDiv.innerHTML = `
          <div class="bg-base-100 ">
@@ -73,7 +121,7 @@ const displayVideo = (videos) => {
                 <div>
                     <h2 class="card-title font-bold">${video.title}</h2>
                     <div class="flex gap-3"> <p class="text-sm text-gray-600">${video.authors[0].profile_name}</p>
-                        <img class="w-5" src="./assets/icons8-verified-badge-32.png" alt="">
+                    ${video.authors[0].verified == true ? `<img class="w-5" src="./assets/icons8-verified-badge-32.png" alt="">` : ``} 
                     </div>
                    
                     <p class="text-sm text-gray-600">${video.others.views}views</p>
@@ -84,12 +132,18 @@ const displayVideo = (videos) => {
               
                  
               </div>
+              <button onclick="loadVideoByDetail('${video.video_id}')" class="btn btn-block">Show details</button>
             </div>
           
         `;
     videoContainer.append(videoDiv);
+    hideLoader();
   });
 };
+document.getElementById("search").addEventListener("keyup",(e) =>{
+const input = e.target.value;
+ loadVideo(input);
+})
 
 loadCategories();
  
